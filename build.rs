9ff97d01dd9b1e252d4dd354a2af19eab9a79d82
Shantanu Goel <shantanu.goel@gmail.com>
2025-05-02 03:15:58 +0530

    fix scale factor fml

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
         slint_build::CompilerConfiguration::new()
             .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer)
             .with_sdf_fonts(true)
-            .with_scale_factor(2.0),
+            .with_scale_factor(1.0),
     )
     .unwrap();
 }
