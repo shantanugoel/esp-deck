ade4b563f09b21b39271ac7e275ed7113f4dac65
Shantanu Goel <shantanu.goel@gmail.com>
2025-05-13 20:24:54 +0530

    fix mouse move editing

--- webapp/src/components/MouseMoveEditor.vue
+++ webapp/src/components/MouseMoveEditor.vue
@@ -3,39 +3,44 @@ import { ref, watch, nextTick } from 'vue'
 import { defineProps, defineEmits } from 'vue'
 const props = defineProps<{ action: any }>()
 const emit = defineEmits(['update'])
-const isEditing = ref(false)
+const isEditingDx = ref(false)
+const isEditingDy = ref(false)
 const tempDx = ref(props.action.MouseMove.dx)
 const tempDy = ref(props.action.MouseMove.dy)
 const dxInputRef = ref<HTMLInputElement | null>(null)
+const dyInputRef = ref<HTMLInputElement | null>(null)
 
-function startEdit() {
+function startEditDx() {
   tempDx.value = props.action.MouseMove.dx
+  isEditingDx.value = true
+  nextTick(() => dxInputRef.value?.focus())
+}
+function saveEditDx() {
+  emit('update', { ...props.action, MouseMove: { dx: tempDx.value, dy: props.action.MouseMove.dy } })
+  isEditingDx.value = false
+}
+function startEditDy() {
   tempDy.value = props.action.MouseMove.dy
-  isEditing.value = true
+  isEditingDy.value = true
+  nextTick(() => dyInputRef.value?.focus())
 }
-function saveEdit() {
-  emit('update', { ...props.action, MouseMove: { dx: tempDx.value, dy: tempDy.value } })
-  isEditing.value = false
+function saveEditDy() {
+  emit('update', { ...props.action, MouseMove: { dx: props.action.MouseMove.dx, dy: tempDy.value } })
+  isEditingDy.value = false
 }
-
-watch(isEditing, (val) => {
-  if (val) nextTick(() => dxInputRef.value?.focus())
-})
 </script>
 <template>
   <div class="flex gap-2 items-center">
     <span class="font-mono">dx:</span>
-    <template v-if="!isEditing">
+    <template v-if="!isEditingDx">
       <span>{{ props.action.MouseMove.dx }}</span>
-      <span class="font-mono ml-2">dy:</span>
-      <span>{{ props.action.MouseMove.dy }}</span>
       <span
         class="ml-1 cursor-pointer text-muted-foreground hover:text-primary"
-        @click="startEdit"
-        title="Edit"
+        @click="startEditDx"
+        title="Edit dx"
         tabindex="0"
         role="button"
-        aria-label="Edit"
+        aria-label="Edit dx"
       >✏️</span>
     </template>
     <template v-else>
@@ -44,16 +49,30 @@ watch(isEditing, (val) => {
         v-model.number="tempDx"
         type="number"
         class="border rounded px-2 py-1 w-14"
-        @keyup.enter="saveEdit"
-        @blur="saveEdit"
+        @keyup.enter="saveEditDx"
+        @blur="saveEditDx"
       />
-      <span class="font-mono ml-2">dy:</span>
+    </template>
+    <span class="font-mono ml-2">dy:</span>
+    <template v-if="!isEditingDy">
+      <span>{{ props.action.MouseMove.dy }}</span>
+      <span
+        class="ml-1 cursor-pointer text-muted-foreground hover:text-primary"
+        @click="startEditDy"
+        title="Edit dy"
+        tabindex="0"
+        role="button"
+        aria-label="Edit dy"
+      >✏️</span>
+    </template>
+    <template v-else>
       <input
+        ref="dyInputRef"
         v-model.number="tempDy"
         type="number"
         class="border rounded px-2 py-1 w-14"
-        @keyup.enter="saveEdit"
-        @blur="saveEdit"
+        @keyup.enter="saveEditDy"
+        @blur="saveEditDy"
       />
     </template>
   </div>
