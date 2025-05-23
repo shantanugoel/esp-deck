1f86e16c3e4922eaf45fc3bf8f5c06801c9b9d27
Shantanu Goel <shantanu.goel@gmail.com>
2025-05-24 00:40:17 +0530

    Remove old webapp code since the new one works fine now

--- webappv1/src/components/UnknownEditor.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { defineProps } from 'vue'
-defineProps<{ action: any }>()
-</script>
-<template>
-  <div class="text-destructive">Unknown action</div>
-</template> 
